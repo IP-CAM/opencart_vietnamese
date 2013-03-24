13df2d1889953ca7992881509d619c66787d0785
Fanha Giang <fanha99@hotmail.com>
2013-03-24 20:18:27 +0700

    cap nhat 1 so tu

--- upload/catalog/language/vietnamese/module/cart.php
+++ upload/catalog/language/vietnamese/module/cart.php
@@ -4,7 +4,9 @@ $_['heading_title'] = 'Giỏ hàng';
 
 // Text
 $_['text_items']    = '%s sản phẩm(s) - %s';
-$_['text_empty']    = 'Giỏ hàng của bạn đang trống!';
-$_['text_cart']     = 'View Cart';
-$_['text_checkout'] = 'Checkout';
+$_['text_empty']    = 'Giỏ hàng đang trống!';
+
+$_['text_cart']     = 'Xem giỏ hàng';
+
+$_['text_checkout'] = 'Thanh toán';
 ?>
